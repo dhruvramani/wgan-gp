df5cac3faf9b575285f89e7b19eda17b90345e7e
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-01-14 19:01:29 +0530

    python3 fix

--- tflib/cifar10.py
+++ tflib/cifar10.py
@@ -28,5 +28,4 @@ def cifar_generator(filenames, batch_size, data_dir):
 
 
 def load(batch_size, data_dir):
-    return cifar_generator(['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5'], batch_size, data_dir), 
-        cifar_generator(['test_batch'], batch_size, data_dir)
+    return cifar_generator(['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5'], batch_size, data_dir),  cifar_generator(['test_batch'], batch_size, data_dir)
